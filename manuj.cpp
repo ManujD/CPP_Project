1ca61dc0c6f9e09bf04bfefc74e6f4cc7cc4a2a4
Rohit Kumar <rohitkr0807@gmail.com>
2020-06-04 15:34:27 +0530

    Updated vector

--- manuj.cpp
+++ manuj.cpp
@@ -47,7 +47,7 @@ int main()
 //     }
 //     cout << endl;
 
-vector<vector<int>> A = {{1,3,5,7},{10, 11, 16, 20},{23, 30, 34, 50}};
+vector<vector<int>> A = {{1,3,5,7},{10, 11, 16, 20},{23, 30, 34, 51}};
 int B  = 55;
 searchMatrix(A,B);
 cout << "A" << " "<<searchMatrix(A,B);
